35c9382b3e09201dad523441bd6002ae9c6cb767
Nibor9198 <robin.boregrim@gmail.com>
2016-04-02 14:19:50 +0200

    Bajs i analen

--- JavaScript.js
+++ JavaScript.js
@@ -3,8 +3,9 @@ function dropDown(s){
     s = '.' + s;
     $(s).attr("color", "red");
     $(s).attr("height", "20px");
-    
-       
-    
     //document.getElementsByClassName(s).setAttribute("visibility", "visible")
 }
+$(function() {
+    var t = ($(document).width()) - (($("#img").outerWidth()) + ($("#menu").outerWidth()));
+    $("#menu").attr("left", t)
+});
