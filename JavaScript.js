8ce5245ec5f8b8280b90cfaaa6a540de1c0778a0
Nibor9198 <robin.boregrim@gmail.com>
2016-04-27 15:16:09 +0200

    Stuff

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,8 @@
 function dropDown(s){
-    $("body").attr("background-color", "red"); 
-    s = '.' + s;
-    $(s).attr("color", "red");
-    $(s).attr("height", "20px");
+    //$("body").attr("background-color", "red"); 
+    //s = '.' + s;
+    //$(s).attr("color", "red");
+    //$(s).attr("height", "20px");
     //document.getElementsByClassName(s).setAttribute("visibility", "visible")
 }
 $(function() {
